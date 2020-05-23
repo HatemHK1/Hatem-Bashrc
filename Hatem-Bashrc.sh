550fbc2023b7494df0434d93c0a94b8ccee0f62e
HatemHK1 <64265896+HatemHK1@users.noreply.github.com>
2020-05-23 03:48:53 +0200

    Add files via upload

---
+++ Hatem-Bashrc.sh
@@ -0,0 +1,110 @@
+	echo "Plese Wait Installing [y/n]"
+read x
+if [ $x == y ]
+then
+sleep 0.1
+clear
+        echo "o.............."
+sleep 0.1
+clear
+        echo "o0............."
+sleep 0.1
+clear
+        echo "o0o............"
+sleep 0.1
+clear
+        echo "o0o0..........."
+sleep 0.1
+clear
+        echo "o0o0o.........."
+sleep 0.1
+clear
+        echo "o0o0o0........."
+sleep 0.1
+clear
+        echo "o0o0o0o........"
+sleep 0.1
+clear
+        echo "o0o0o0o0......."
+sleep 0.1
+clear
+        echo "o0o0o0o0o......"
+sleep 0.1
+clear
+        echo "o0o0o00o0o....."
+sleep 0.1
+clear
+	echo "what's your name............?"
+read user
+sleep 3
+        echo "Welcome $user"
+leafpad .bashrc
+        echo "E.................................\ "
+sleep 0.1
+clear
+        echo "EX................................/ "
+sleep 0.1
+clear
+        echo "EXI...............................\ "
+sleep 0.1
+clear
+        echo "EXIT............................../ "
+sleep 0.1
+clear
+        echo "EXIT H............................\ "
+sleep 0.1
+clear
+        echo "EXIT Ha.........................../ "
+sleep 0.1
+clear
+        echo "EXIT Hat..........................\ "
+sleep 0.1
+clear
+        echo "EXIT Hate........................./ "
+sleep 0.1
+clear
+        echo "EXIT Hatem.......................\ "
+sleep 0.1
+clear
+        echo "EXIT Hatem H....................../ "
+sleep 0.1
+clear
+        echo "EXIT Hatem K.......................\ "
+sleep 0.1
+clear
+        echo "E.................................\ "
+sleep 0.1
+clear
+        echo "EX................................/ "
+sleep 0.1
+clear
+        echo "EXI...............................\ "
+sleep 0.1
+clear
+        echo "EXIT............................../ "
+sleep 0.1
+clear
+        echo "EXIT H............................\ "
+sleep 0.1
+clear
+        echo "EXIT Ha.........................../ "
+sleep 0.1
+clear
+        echo "EXIT Hat..........................\ "
+sleep 0.1
+clear
+        echo "EXIT Hate........................./ "
+sleep 0.1
+clear
+        echo "EXIT Hatem.......................\ "
+sleep 0.1
+clear
+        echo "EXIT Hatem H....................../ "
+sleep 0.1
+clear
+        echo "EXIT Hatem K.......................\ "
+sleep 0.1
+clear
+fi
+	echo "Please ctrl+c >>> exit "
+read exit
